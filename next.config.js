b64968e3ec77d22dc381a1b2e1e06ede65e3ec64
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-09-21 17:12:49 +0200

    fix: assetPrefix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? '/0xa-at/website' : '',
+  assetPrefix: isProd ? '/website' : '',
   images: {
     unoptimized: true,
   },
