6d469309c758cbd6f2b89acf2b39326e66dbe4ad
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-09-23 18:02:27 +0200

    style: minor changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,26 +9,19 @@ export default function AboutPage() {
             <PageHeader title={"About us"} />
 
             <Typography pt={3} variant="h4">What we do</Typography>
-
             <Typography variant="body1">TBD</Typography>
 
-
             <Typography pt={3} variant="h4">Leadership</Typography>
-
-
             <Box pt={5} sx={{ display: 'flex', justifyContent: 'space-around' }}>
                 <Person name={"Alexander Peherstorfer"} role={"Chairman"} />
                 <Person name={"Severin Mairinger"} role={"Chairman deputy"} />
             </Box>
-
-
             <Box pt={10} sx={{ display: 'flex', justifyContent: 'space-around' }}>
                 <Person name={"Matthias Heiden"} role={"Secretary"} />
                 <Person name={"Andreas Schmid"} role={"Secretary deputy"} />
                 <Person name={"Alexander Brunner"} role={"Accountant"} />
                 <Person name={"Tobias Pucher"} role={"Accountant deputy"} />
             </Box>
-
         </Box>
     )
 }
