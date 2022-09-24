e31a1161fc95d09dd5ba63d75e1095f0ba5417b0
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-09-24 10:01:58 +0200

    fix: build

--- components/PostMetadata.tsx
+++ components/PostMetadata.tsx
@@ -1,7 +1,7 @@
 import { Link, Typography } from "@mui/material";
 import { AuthorType } from "../utils/types";
 
-export default function PostMetadata({ date, author }: { date: string, author: AuthorType }) {
+export default function PostMetadata({ date, author }: { date: Date, author: AuthorType }) {
     return (
         <Typography variant="subtitle1">
             {date.toString()} by <Link href={`/author/${author.id}`}>{author.name}</Link>
--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -1,13 +1,13 @@
-import { Link, Typography } from "@mui/material"
+import { Link } from "@mui/material"
 import { Box } from "@mui/system"
-import PostType from "../utils/types"
+import PostType, { AuthorType } from "../utils/types"
 import PostMetadata from "./PostMetadata"
 
 export default function PostPreview({ post }: { post: PostType }) {
     return (
         <Box m={5}>
-            <Link variant="h5" href={`/posts/${post.slug}`}>{post.title}</Link>
-            <PostMetadata date={post.date} author={post.author} />
+            <Link variant="h5" href={`/posts/${post.id}`}>{post.title}</Link>
+            <PostMetadata date={post.date} author={post.author as AuthorType} />
         </Box>
     )
 }
