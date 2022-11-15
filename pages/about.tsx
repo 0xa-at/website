f6ed476404df733fcc018ab3c6a14f3d6f4ed774
Sev <sevm@pm.me>
2022-11-15 15:57:57 +0100

    updated text passages and favicon

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,16 +9,18 @@ export default function AboutPage() {
   return (
     <Box>
       <PageHeader title={"About us"} />
-
-      <Typography pt={3} variant="h4">
-        What we do
+      <Typography pt={4} variant="h3">
+        0xA Science Association
+      </Typography>
+      <hr></hr>
+      <Typography pt={1} variant="h5">
+        Association for the advancement of science and technology in society
       </Typography>
       <Typography variant="body1">
-        0xA Science Association - Association for the advancement of science and
-        technology in society<br></br>
+        <br></br>
         <br></br>
         <b>
-          <i>"What?"</i>
+          <i>"What we do?"</i>
         </b>
         <br></br>
         <br></br>
@@ -26,7 +28,7 @@ export default function AboutPage() {
         <br></br>
         <br></br>
         <b>
-          <i>"Who?"</i>
+          <i>"Who we are for?"</i>
         </b>
         <br></br>
         <br></br>
@@ -34,42 +36,42 @@ export default function AboutPage() {
         networking amongst competent people.
         <br></br>
         <br></br>
-        <p>
-          As our name suggests, we are committed to foster science and
-          technology. But what do we mean by that?<br></br>
-          <ul>
-            <li>
-              On the one hand, we offer opportunities to advance your knowledge,
-              particularly in technical domains.
-            </li>
-            <li>
-              On the other hand, we provide a platform to present your own
-              knowledge to a competent and interested audience.
-            </li>
-          </ul>
-        </p>
-        <p>
-          We organize presentations, where our members can shine with their
-          competence and share that knowledge with interested listeners. A
-          win-win situation. These presentations are recorded and available
-          online afterwards.
-          <br />
-          We offer a platform for our members to write blogposts on scientific
-          topics and make them available to a interested community to read,
-          discuss and learn. We plan workshops and projects, to not leave
-          practice out of the equation. <br /> A fair share between theory and practice
-          is important to us, and a presentation on this very topic will be the
-          kick-off at our launch-event.
-          <br /><br />
-          Another mission of 0xA is to connect
-          people. We will create a network that enables its members to establish
-          competent contacts fast and easily. This is why we will also foster
-          the conversion of common projects, e.g. through cooperations with
-          companies or by providing know-how. In the future will therefore arise
-          a colourful mix of presentations, workshops, blogposts and other
-          events on various topics, and we are already looking forward to Your
-          contribution.
-        </p>
+      </Typography>
+      <div>
+        As our name suggests, we are committed to foster science and technology.
+        But what do we mean by that?<br></br>
+        <ul>
+          <li>
+            On the one hand, we offer opportunities to advance your knowledge,
+            particularly in technical domains.
+          </li>
+          <li>
+            On the other hand, we provide a platform to present your own
+            knowledge to a competent and interested audience.
+          </li>
+        </ul>
+      </div>
+      <Typography>
+        We organize presentations, where our members can shine with their
+        competence and share that knowledge with interested listeners. A win-win
+        situation. These presentations are recorded and available online
+        afterwards.
+        <br />
+        We offer a platform for our members to write blogposts on scientific
+        topics and make them available to a interested community to read,
+        discuss and learn. We plan workshops and projects, to not leave practice
+        out of the equation. <br /> A fair share between theory and practice is
+        important to us, and a presentation on this very topic will be the
+        kick-off at our launch-event.
+        <br />
+        <br />
+        Another mission of 0xA is to connect people. We will create a network
+        that enables its members to establish competent contacts fast and
+        easily. This is why we will also foster the conversion of common
+        projects, e.g. through cooperations with companies or by providing
+        know-how. In the future will therefore arise a colourful mix of
+        presentations, workshops, blogposts and other events on various topics,
+        and we are already looking forward to Your contribution.
       </Typography>
 
       <Typography pt={3} variant="h4">
@@ -85,7 +87,7 @@ export default function AboutPage() {
         }}
       >
         <Person name={"Alexander Peherstorfer"} role={"Chairman"} />
-        <Person name={"Severin Mairinger"} role={"Chairman deputy"} />
+        <Person name={"Severin Mairinger"} role={"Vice-Chairman"} />
       </Box>
       <Box
         pt={10}
@@ -97,9 +99,9 @@ export default function AboutPage() {
         }}
       >
         <Person name={"Matthias Heiden"} role={"Secretary"} />
-        <Person name={"Andreas Schmid"} role={"Secretary deputy"} />
+        <Person name={"Andreas Schmid"} role={"Vice-Secretary"} />
         <Person name={"Alexander Brunner"} role={"Accountant"} />
-        <Person name={"Tobias Pucher"} role={"Accountant deputy"} />
+        <Person name={"Tobias Pucher"} role={"Vice-Accountant"} />
       </Box>
     </Box>
   );
