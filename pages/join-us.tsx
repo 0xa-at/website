4bf114955dbbd3db0c31eddc409386a57bc6f7a1
not-matthias <26800596+not-matthias@users.noreply.github.com>
2023-04-06 16:11:06 +0200

    chore: start working on join us page

---
+++ pages/join-us.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import PageHeader from "../components/PageHeader";
+
+export default function JoinUsPage() {
+    return (
+        <Box>
+            <PageHeader title="Join us!" />
+
+            TBD
+        </Box>
+    )
+}
